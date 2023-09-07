9b03984ed33d0288a7d2840129820e94c2f04abe
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-09-06 20:38:08 -0400

    Ignore modtime for deploy sync
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -10,4 +10,4 @@ export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
 export BASEURL="$URL"
 
 HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $BASEURL
-rclone sync --progress public/ wtgdeml-wtg:./
+rclone sync --checksum --progress public/ wtgdeml-wtg:./
